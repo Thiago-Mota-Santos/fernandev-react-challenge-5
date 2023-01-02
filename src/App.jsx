4cd039a00ca2f02d2b16d126d409f2e68994a4e8
Thiago Mota Santos <lucimara1422@gmail.com>
2023-01-02 11:40:25 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
+import axios from 'axios'
+
 
 /*
 Consuma a API e liste todos os pokemons da consulta do seguinte endpoint. 
@@ -21,14 +24,52 @@ EXTRA: se puder ordene por nome.
 */
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [list, setList] = useState([]);
+
+  useEffect(() =>{
+    axios.get('https://pokeapi.co/api/v2/pokemon').then(response => setList(response.data.results));
+  },[])
+
+  
 
   return (
     <>
-      <h3>desafio fernandev</h3>
-      <h1>consumir api pokémon</h1>
+      <h3>Listagens de pokemons</h3>
+      {
+        list.map(pokemon => 
+        <p key={pokemon.name}>
+          <PokemonMoreDetails data={pokemon}/>
+        </p>)
+      }
     </>
   );
+}
+
+const PokemonMoreDetails = ({data}) => {
+  const [moreDetails, setMoreDetails] = useState('');
+
+  useEffect(() => {
+    axios.get(data.url).then(response => setMoreDetails(response.data))
+  }, [])
+
+
+  if(moreDetails === ''){
+    return (
+      <p>---</p>
+    )
+  }
+
+  return(
+    <div style={{display: 'flex', alignItens: 'center', }}>
+    
+    <img src={moreDetails.sprites.front_default} alt='pokemon image' style={{width: 30, marginRight: 5}}></img>
+    <p>{moreDetails.name} - {moreDetails.base_experience} EXP</p>
+    
+    </div>
+   
+  )
+
 }
 
 export default App;
